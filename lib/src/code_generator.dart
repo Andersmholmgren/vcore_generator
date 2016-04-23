23c2ae2e60614d1e55a4d02e1a8c4e8794b8fe20
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-23 17:17:13 +1000

    code gen

--- lib/src/code_generator.dart
+++ lib/src/code_generator.dart
@@ -30,13 +30,26 @@ import 'package:built_value/built_value.dart';
   }
 
   void generateClass(ValueClass valueClass, IOSink sink) {
-    sink.write('abstract class ${valueClass.name} '
-        'implements Built<${valueClass.name}, ${valueClass.name}Builder>');
+    final className = valueClass.name;
+    final classNameLower =
+        className.substring(0, 1).toLowerCase() + className.substring(1);
+    sink.write('abstract class $className '
+        'implements Built<$className, ${className}Builder>');
     final superNames = valueClass.superTypes.map((c) => c.name);
     if (superNames.isNotEmpty) {
       sink..write(', ')..write(superNames.join(', '));
     }
     sink.writeln(' {');
+
+    sink.writeln('static final Serializer<$className> serializer'
+        ' = _\$${classNameLower}Serializer;');
+    sink.writeln();
+
+    sink..writeln('$className._();')..writeln();
+
+    sink.writeln(
+        'factory $className([updates(${className}Builder b)]) = _\$$className;');
+    sink.writeln();
     sink.writeln('}');
   }
 }
