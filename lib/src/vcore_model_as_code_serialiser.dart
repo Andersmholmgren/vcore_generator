adf89dc369090023af6ad9b6809891f46183f9d8
Anders Holmgren <andersmholmgren@gmail.com>
2016-05-01 10:19:39 +1000

    vcore model generator generate code for serialised model instead of json serialisation

--- lib/src/vcore_model_as_code_serialiser.dart
+++ lib/src/vcore_model_as_code_serialiser.dart
@@ -20,5 +20,52 @@ Package _create${name}Package() {
     return packageBuilder.build();
   }
     ''');
+
+    package.classifiers.forEach((c) => _serialiseClassifier(c, sink));
   }
+
+  void _serialiseClassifier(Classifier c, StringSink sink) {
+    if (c is ValueClass) {
+      _serialiseClass(c, sink);
+    } else {
+      sink.writeln('// TODO: support ${c.runtimeType}');
+    }
+  }
+
+  void _serialiseClass(ValueClass vc, StringSink sink) {
+    final name = vc.name;
+    final capName = _capitalise(name);
+    sink.writeln('''
+ValueClass _${name};
+ValueClass get ${name} => _${name} ??= _create${capName}();
+
+ValueClass _create${capName}() {
+  return new ValueClass((cb) => cb
+    ..name = '${capName}'
+    ..isAbstract = ${vc.isAbstract}
+    ..superTypes.addAll(${[vc.superTypes.map((t) => t.name).join(', ')]})
+    ''');
+
+    vc.properties.forEach((p) {
+      sink.writeln('''
+    ..properties.add(new Property((b) => b
+      ..name = '${p.name}'
+      ..type = ${p.type.name}
+      ..isNullable = ${p.isNullable}
+      ..derivedExpression = ${p.derivedExpression}
+      ..docComment = ${p.docComment}
+      ..defaultValue = ${p.defaultValue})
+    )
+      ''');
+    });
+
+    sink.writeln('''
+      );
+}
+''');
+  }
+}
+
+String _capitalise(String s) {
+  return s.substring(0, 1).toUpperCase() + s.substring(1);
 }
