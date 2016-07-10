54740206e0be55cd6fc2a8596ae15948eeed0563
Anders Holmgren <andersmholmgren@gmail.com>
2016-07-10 10:57:11 +1000

    capture builder type

---
+++ lib/src/type_name.dart
@@ -0,0 +1,101 @@
+class TypeName {
+  final String baseName;
+  final Iterable<TypeName> typeParameters;
+
+//,  fullTypeName;
+
+//  if (typeName != 'Built' && !typeName.startsWith('Serializer')) {
+//    final _ResolvingClassifierHelper classifierHelper =
+//      _classifierHelpers[fullTypeName] ?? _classifierHelpers[typeName];
+//    if (classifierHelper == null) {
+//      final coreType = _coreTypes[typeName];
+//      if (coreType != null) {
+//        return new _ResolvedExternalClassifier(coreType);
+//      } else {
+  bool get isSet => baseName == 'BuiltSet';
+  bool get isList => baseName == 'BuiltList';
+  bool get isCollection => isSet || isList;
+  bool get isMap => baseName == 'BuiltMap';
+  bool get isMultiValued => isCollection || isMap;
+
+  TypeName(this.baseName, {this.typeParameters: const []});
+
+  factory TypeName.parse(String fullTypeName) {
+    final startIndex = fullTypeName.indexOf('<');
+    if (startIndex < 0) return new TypeName(fullTypeName);
+
+    final baseName = fullTypeName.substring(0, startIndex);
+
+    final endIndex = fullTypeName.lastIndexOf('>');
+    if (endIndex < 0) throw new ArgumentError('$fullTypeName has missing >');
+
+    final typeParamNames = fullTypeName
+        .substring(startIndex + 1, endIndex)
+        .split(',')
+        .map((s) => s.trim());
+    final typeParameters = typeParamNames.map((p) => new TypeName.parse(p));
+
+    return new TypeName(baseName, typeParameters: typeParameters);
+  }
+
+  String toString() {
+    final paramString = typeParameters.isEmpty
+        ? ''
+        : '<${typeParameters.map((t) => t.toString()).join(', ')}>';
+    return '$baseName$paramString';
+  }
+
+//  Iterable<String>
+
+//        if (isMultiValued) {
+//          print('found new multivalued type $fullTypeName');
+//          final typeParamNames = fullTypeName
+//            .substring(fullTypeName.indexOf('<') + 1,
+//            fullTypeName.lastIndexOf('>'))
+//            .split(',')
+//            .map((s) => s.trim());
+//          final typeParamHelpers = typeParamNames.map((typeParamFullName) {
+//            print('typeParamFullName: $typeParamFullName');
+//
+//            final i = typeParamFullName.indexOf('<');
+//            final typeParamName =
+//            i < 0 ? typeParamFullName : typeParamFullName.substring(0, i);
+//
+//            return _resolveHelperByName(typeParamName, typeParamFullName);
+//          });
+//          print('*** $typeParamHelpers for $typeParamNames');
+//
+//          if (isCollection) {
+//            final typeParamHelper = typeParamHelpers.first;
+//
+//            final typeBuilder = isSet
+//              ? createBuiltSet(typeParamHelper.resolvingClassifier)
+//              : createBuiltList(typeParamHelper.resolvingClassifier);
+//            final helper = new _ResolvingGenericTypeClassifier(typeBuilder);
+//            _classifierHelpers[fullTypeName] = helper;
+//            print('added: $fullTypeName -> $helper');
+//            return helper;
+//          } else if (isMap) {
+//            final typeBuilder = createBuiltMap(
+//              typeParamHelpers.first.resolvingClassifier,
+//              typeParamHelpers.elementAt(1).resolvingClassifier);
+//            final helper = new _ResolvingGenericTypeClassifier(typeBuilder);
+//            _classifierHelpers[fullTypeName] = helper;
+//            print('added: $fullTypeName -> $helper');
+//            return helper;
+//          }
+//        } else {
+////          throw new StateError(
+////              "failed to resolve classifier helper class: $type");
+//          print("failed to resolve classifier helper class: $typeName");
+//          return new _ResolvedExternalClassifier(
+//            (new ExternalClassBuilder()..name = typeName).build());
+//        }
+//      }
+//    } else {
+//      return classifierHelper;
+//    }
+//  } else {
+//    return null;
+//  }
+}
